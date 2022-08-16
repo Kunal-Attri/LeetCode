9e08722e2036ca9d7b21c1002f3f785a25f24085
Kunal Attri <attrikunal16@gmail.com>
2022-08-17 00:45:12 +0530

    problem 2188

--- src/leetcode/PrintFooBarAlternately.java
+++ src/leetcode/PrintFooBarAlternately.java
@@ -1,5 +1,39 @@
 package leetcode;
 
-public class PrintFooBarAlternately {
+import java.util.concurrent.atomic.AtomicBoolean;
 
+public class PrintFooBarAlternately {
+	private int n;
+    AtomicBoolean hasDoneFirst = new AtomicBoolean(false);
+
+    public PrintFooBarAlternately(int n) {
+        this.n = n;
+    }
+    
+    public void foo(Runnable printFoo) throws InterruptedException {
+
+        synchronized(this) {
+            for (int i = 0; i < n; i++) {
+                while (hasDoneFirst.get()) {
+                    wait();
+                }
+                printFoo.run();
+                hasDoneFirst.set(true);
+                notify();
+            }
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        synchronized(this) {
+            for (int i = 0; i < n; i++) {
+                while(!hasDoneFirst.get()) {
+                    wait();
+                }
+                printBar.run();
+                hasDoneFirst.set(false);
+                notify();
+            }
+        }
+    }
 }
